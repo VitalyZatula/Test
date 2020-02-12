65781ff15b0ada19fd251f273ce96a92ed5648ee
VitalyZatula <vitalyzawork@gmail.com>
2020-02-12 20:47:21 +0200

    Allure changes

--- target/classes/tests/BaseTest.java
+++ target/classes/tests/BaseTest.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestContext;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
@@ -18,14 +19,15 @@ public class BaseTest {
 	WebDriver driver;
 
 	@BeforeClass
-	public void setup() {
+	public void setup(ITestContext testContext) {
 	//	System.setProperty("webdriver.chrome.driver","C:\\Users\\Vitaly\\Desktop\\Automation\\Drivers\\chromedriver.exe");
 		driver= new ChromeDriver();
+		testContext.setAttribute("WebDriver", this.driver);
 		driver.get("https://events.eply.com/login/index.aspx");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	}
-
+/*
 	@AfterMethod
 	public void failedTest(ITestResult result) throws InterruptedException {
 		//check if test failed
@@ -41,19 +43,13 @@ public class BaseTest {
 		}
 	}
 
-
+*/
 
 
 	@AfterClass
 	public void teardown() {
 		driver.quit();
 	}
-
-
-
-
-
-
 }
 
 
--- target/classes/tests/SaveAccountData.java
+++ target/classes/tests/SaveAccountData.java
@@ -3,10 +3,18 @@ package tests;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Issue;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.Story;
 import pageObject.AccountPage;
 import pageObject.LoginPage;
 import pageObject.ViewMyFormsPage;
 
+@Epic("Login and Update Contact Data")
+@Feature( "User Details")
 public class SaveAccountData extends BaseTest{
 
 
@@ -18,7 +26,11 @@ public class SaveAccountData extends BaseTest{
 		String expected ="Hello, Shavit!";
 		String actual =login.getWellcomeMsg();
 		Assert.assertEquals(actual, expected);
-	}		
+	}
+	
+	@Severity(SeverityLevel.CRITICAL)
+	@Story("I as a user would like to update my contact information ")
+	@Issue("2")
 	@Test
 	public void saveAcounData() throws InterruptedException {
 		ViewMyFormsPage formsPage=new ViewMyFormsPage (driver);
